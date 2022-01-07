64c3715ea797cff56559c31ed9664ad136a69f7a
Lawrence Dovin <lawrencedovin@gmail.com>
2022-01-07 18:59:00 -0500

    Updated components for section 4

--- 4-extra-features-with-directives/src/app/app.module.ts
+++ 4-extra-features-with-directives/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NgForLoopComponent } from './ng-for-loop/ng-for-loop.component';
+import { NgClassComponent } from './ng-class/ng-class.component';
+import { NgIfComponent } from './ng-if/ng-if.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NgForLoopComponent,
+    NgClassComponent,
+    NgIfComponent
   ],
   imports: [
     BrowserModule
---
+++ 4-extra-features-with-directives/src/app/ng-for-loop/ng-for-loop.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ng-for-loop',
+  templateUrl: './ng-for-loop.component.html',
+  styleUrls: ['./ng-for-loop.component.css']
+})
+export class NgForLoopComponent {
+  groceries = ['Apples', 'Grapes', 'Potatoes'];
+}
