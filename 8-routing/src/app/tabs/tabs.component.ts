31b98055319f6705d1e2a571cea08c2b98b0e0cb
Lawrence Dovin <lawrencedovin@gmail.com>
2022-01-20 14:57:44 -0500

    Added initial routing

---
+++ 8-routing/src/app/tabs/tabs.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { StarWarsService } from '../star-wars.service';
+import { Person } from '../interfaces/person.interface';
+
+@Component({
+  selector: 'app-tabs',
+  templateUrl: './tabs.component.html',
+  styleUrls: ['./tabs.component.css']
+})
+export class TabsComponent {
+  characters: Array<Person> = [];
+  chosenList = 'all';
+
+  constructor(private starWarsService: StarWarsService) {}
+
+  onChoose(side: string) {
+    this.chosenList = side;
+  }
+
+  getCharacters() {
+      this.characters = this.starWarsService.getCharacters(this.chosenList);
+      return this.characters;
+  }
+
+}
