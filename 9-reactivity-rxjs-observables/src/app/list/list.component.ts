743a38071e22fdb20da37005402e75e493225c4f
Lawrence Dovin <lawrencedovin@gmail.com>
2022-01-21 15:03:55 -0500

    Added ngOnDestroy to unsubscribe from Subject

--- 9-reactivity-rxjs-observables/src/app/list/list.component.ts
+++ 9-reactivity-rxjs-observables/src/app/list/list.component.ts
@@ -1,21 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { StarWarsService } from '../star-wars.service';
 import { Person } from '../interfaces/person.interface';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.css']
 })
-export class ListComponent implements OnInit{
+export class ListComponent implements OnInit, OnDestroy{
   // Gets characters from tabs
   characters: Array<Person>;
   // Used to track changes to side depending on route
   loadedSide = 'all';
 
+  // Manage our subscriptions for our own created Subjects.
+  subscription: Subscription;
+
   constructor(private activatedRoute: ActivatedRoute, private starWarsService: StarWarsService) {}
 
+  // Executes whenever this component is created
   ngOnInit() {
     this.activatedRoute.params.subscribe(
       // (params) is a function which receives the updated params which we can now handle
@@ -27,11 +32,16 @@ export class ListComponent implements OnInit{
         this.loadedSide = params['side'];
       }
     );
-    this.starWarsService.charactersChanges.subscribe(
+    this.subscription = this.starWarsService.charactersChanges.subscribe(
       () => {
         // Gets the newly updated characters list from starwars service when side changes.
         this.characters = this.starWarsService.getCharacters(this.loadedSide);
       }
     )
+  }
+
+  // Executes whenever this component is about to get destroyed
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 }
